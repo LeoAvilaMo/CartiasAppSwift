22a72b3db0c6204d993bc7001350f31dd3660396
Ultimate553 <diego.torred@gmail.com>
2024-08-27 07:45:29 -0600

    xcode 15.4 v

---
+++ Caritas_iBorregOS/Caritas_iBorregOS/Caritas_iBorregOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caritas_iBorregOSApp.swift
+//  Caritas_iBorregOS
+//
+//  Created by Diego Torre on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Caritas_iBorregOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
