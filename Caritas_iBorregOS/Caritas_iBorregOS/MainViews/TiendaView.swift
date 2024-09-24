2ddfa468950d4b810bffbb50b33cc2e65e9a695d
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2024-09-24 17:48:14 -0600

    Update TiendaView.swift

--- Caritas_iBorregOS/Caritas_iBorregOS/MainViews/TiendaView.swift
+++ Caritas_iBorregOS/Caritas_iBorregOS/MainViews/TiendaView.swift
@@ -9,50 +9,55 @@ import SwiftUI
 
 struct TiendaView: View {
     var body: some View {
-        VStack {
-            HStack{Spacer()}
-            // Stack con logo y puntos
-            HStack {
-                Spacer()
-                // Stack de puntos
+        NavigationStack{
+            VStack {
+                HStack{Spacer()}
+                // Stack con logo y puntos
                 HStack {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
-                    Text("135")
-                        .font(.system(size: 18, weight: .bold))
-                        .foregroundColor(whiteC)
+                    Spacer()
+                    // Stack de puntos
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                        Text("135")
+                            .font(.system(size: 18, weight: .bold))
+                            .foregroundColor(whiteC)
+                    }
+                    .padding(.vertical, 6)
+                    .padding(.horizontal, 10)
+                    .background(darkBlueC)
+                    .cornerRadius(12)
                 }
-                .padding(.vertical, 6)
-                .padding(.horizontal, 10)
-                .background(darkBlueC)
-                .cornerRadius(12)
-            }
-            .padding(.horizontal)
-            
-            // Titulo
-            Text("Tienda")
-                .font(.system(size: 50))
-                .bold()
-                .foregroundColor(darkBlueC)
-                .padding(.bottom, 20)
-            
-            // Lista
-            VStack{
-                List(listaBeneficios){ beneficio in
-                    BeneficioCardView(beneficioX: beneficio)
+                .padding(.horizontal)
+                
+                // Titulo
+                Text("Tienda")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(darkBlueC)
+                    .padding(.bottom, 20)
+                
+                // Lista
+                VStack{
+                    List(listaBeneficios){ beneficio in
+                        NavigationLink (destination: BeneficioDetailView(beneficioX: beneficio)){
+                            BeneficioCardView(beneficioX: beneficio)
+                        }
+                    }
+                    .listStyle(PlainListStyle())
+                    .scrollIndicators(.hidden)
                 }
-                .listStyle(.inset)
-                .scrollIndicators(.hidden)
+                .background(lightGreenC)
+                .cornerRadius(10)
+                .padding()
+                .offset(y: -10)
             }
-            .background(whiteC)
-            .cornerRadius(10)
-            .padding()
-            .offset(y: -10)
+            .background(lightGreenC)
         }
-        .background(lightGreenC)
     }
 }
 
 #Preview {
     TiendaView()
 }
+
