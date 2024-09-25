e3decf012e32f50656616dda88ae7b54977133a9
Ultimate553 <diego.torred@gmail.com>
2024-09-25 08:51:59 -0600

    removed wrong file

--- Procedures&Triggers.sql
+++ Procedures&Triggers.sql
@@ -1,40 +1,20 @@
 USE iborregos;
-GO
-
-CREATE PROCEDURE sp_AddUsuarioEvento
-    @UsuarioID numeric(18,0),
-    @EventoID numeric(18,0)
-AS
-BEGIN
-    INSERT INTO USUARIOS_EVENTOS (USUARIO, EVENTO)
-    VALUES (@UsuarioID, @EventoID)
+GO CREATE PROCEDURE sp_AddUsuarioEvento @UsuarioID numeric(18, 0),
+    @EventoID numeric(18, 0) AS BEGIN
+INSERT INTO USUARIOS_EVENTOS (USUARIO, EVENTO)
+VALUES (@UsuarioID, @EventoID)
+END
+GO USE iborregos;
+GO CREATE PROCEDURE sp_DeleteUsuarioEvento @UsuarioID numeric(18, 0),
+    @EventoID numeric(18, 0) AS BEGIN
+DELETE FROM USUARIOS_EVENTOS
+WHERE USUARIO = @UsuarioID
+    AND EVENTO = @EventoID
+END
+GO USE iborregos;
+GO CREATE PROCEDURE sp_SelectEvento @EventoID numeric(18, 0) AS BEGIN
+SELECT *
+FROM EVENTOS
+WHERE ID_EVENTO = @EventoID;
 END
 GO
-
-USE iborregos;
-GO
-
-CREATE PROCEDURE sp_DeleteUsuarioEvento
-    @UsuarioID numeric(18,0),
-    @EventoID numeric(18,0)
-AS
-BEGIN
-    DELETE FROM USUARIOS_EVENTOS
-    WHERE USUARIO = @UsuarioID AND EVENTO = @EventoID
-END
-GO
-
-
-USE iborregos;
-GO
-
-CREATE PROCEDURE sp_SelectEvento
-    @EventoID numeric(18,0)
-AS
-BEGIN
-    SELECT *
-    FROM EVENTOS
-    WHERE ID_EVENTO = @EventoID;
-END
-GO
-    
