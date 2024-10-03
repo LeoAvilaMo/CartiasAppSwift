2a0e0198ebde1bfd801d6cdf33907fb3ba78a890
Leonardo Avila <112590593+LeoAvMo@users.noreply.github.com>
2024-10-03 17:10:09 -0600

    Update RetosView.swift

--- Caritas_iBorregOS/Caritas_iBorregOS/MainViews/RetosView.swift
+++ Caritas_iBorregOS/Caritas_iBorregOS/MainViews/RetosView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct RetosView: View {
     var body: some View {
-        Text("Retos View")
+        NavigationStack{
+            VStack(alignment: .leading) {
+                HStack {Spacer()}
+                Spacer()
+                Text("Retos")
+                ScrollView{
+                    NavigationLink{
+                        TiendaView()
+                    } label: {
+                        VStack(alignment: .leading){
+                            RetoCardView()
+                        }
+                        .background(whiteC)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding(.vertical, -1)
+                    }
+                    NavigationLink{
+                        TiendaView()
+                    } label: {
+                        VStack(alignment: .leading){
+                            RetoCardView()
+                        }
+                        .background(whiteC)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding()
+                    }
+                }
+                Spacer()
+            }
+            .background(lightGreenC)
+        }
+        
+        
     }
 }
 
