49a33c1a9a6bc145bea49a206184e5b219a5c714
Ultimate553 <diego.torred@gmail.com>
2024-10-09 11:02:32 -0600

    added puntos functionality

---
+++ Caritas_iBorregOS/Caritas_iBorregOS/MainViews/PuntosView.swift
@@ -0,0 +1,18 @@
+//
+//  PuntosView.swift
+//  Caritas_iBorregOS
+//
+//  Created by Diego Torre on 09/10/24.
+//
+
+import SwiftUI
+
+struct PuntosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PuntosView()
+}
