669ccd5768f2ffbb6d3fda3be4f9ceb59a0268b5
Leonardo Avila <avilamolinaleonardo@gmail.com>
2024-10-10 10:40:36 -0600

    View de PremiosActualizada

---
+++ Caritas_iBorregOS/Caritas_iBorregOS/MainViews/ElementViews/PREMIOSSCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PREMIOSSCardView.swift
+//  Caritas_iBorregOS
+//
+//  Created by Leo A.Molina on 10/10/24.
+//
+
+import SwiftUI
+
+struct PREMIOSSCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PREMIOSSCardView()
+}
